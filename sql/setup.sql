b91c32691c0c0885cc2ddc6630678106daa58a00
Lawson <lawsonmcphetridge2020@gmail.com>
2022-12-05 14:38:54 -0800

    added callback route

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,10 +2,19 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users cascade;
 
+drop table if exists github_users;
+
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
+);
+
+CREATE TABLE github_users (
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+login text not null,
+email text,
+avatar text
 );
